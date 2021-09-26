eee288aeb03fec092ff173e981498dee19edc8ef
Vitaliy Zlobin <zlovian@yandex.ru>
2021-09-26 22:48:21 +0700

    Fix HW08 - XML & JSON

--- HW08 - XML & JSON/hw_xml_json_tasks.sql
+++ HW08 - XML & JSON/hw_xml_json_tasks.sql
@@ -108,7 +108,7 @@ FOR XML PATH('Item'), ROOT('StockItems')
 SELECT	[StockItemID]
 		,[StockItemName]
 		,JSON_VALUE([CustomFields], '$.CountryOfManufacture') AS [CountryOfManufacture]
-		,JSON_VALUE([CustomFields], '$.Tags[1]') AS [FirstTag]
+		,JSON_VALUE([CustomFields], '$.Tags[0]') AS [FirstTag]
 FROM [Warehouse].[StockItems]
 
 /*
