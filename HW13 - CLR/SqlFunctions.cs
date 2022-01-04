28c154ad34d9fb1c245d589232e436c2941eff10
Vitaliy Zlobin <zlovian@yandex.ru>
2022-01-04 14:19:46 +0700

    Add SqlFunctions.cs

---
+++ HW13 - CLR/SqlFunctions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace LibSQL
+{
+    public class SqlFunctions
+    {
+        public static int IsMatched(string str, string pattern) => Regex.IsMatch(str, pattern, RegexOptions.IgnoreCase) ? 1 : 0;
+
+
+        public static int GetRandomNumber(int min, int max) => new Random().Next(min, max);
+
+    }
+}
